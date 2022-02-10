48d8cc4674c25f2980c3fd17259088cff87c341c
Kenya <>
2022-02-10 19:48:20 +0900

    モデル対応

--- reviewapp/models.py
+++ reviewapp/models.py
@@ -7,7 +7,6 @@ class ReviewModel(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField()
     author=models.ForeignKey(User,on_delete=models.CASCADE)
-    images=models.ImageField(upload_to='')
     useful_review=models.IntegerField(null=True, blank=True, default=0)
     useful_review_record=models.TextField()
     evaluations=models.CharField(max_length=10,choices=EVALUATION_CHOICES)
